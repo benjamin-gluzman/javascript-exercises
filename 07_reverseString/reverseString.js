bf1bf8fe90cb0032a76e447707f0e27aebaa0294
Benjamin Gluzman <benjamin.gluzman@gmail.com>
2025-12-04 11:24:44 -0500

    Complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = "";
+    for(let char of str) {
+        result = char + result;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
