5d38cf58980449bd403091dc3a565f05ed33d549
Benjamin Gluzman <benjamin.gluzman@gmail.com>
2025-12-04 11:46:52 -0500

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(typeof start != "number" || typeof end != "number" || Math.floor(start) != start || Math.floor(end) != end) {
+        return 'ERROR';
+    }
+    if(start < 0 || end < 0) return 'ERROR';
+    let sum = 0;
+    for(let i = Math.min(start, end); i <= Math.max(start, end); i++) {
+        sum += i;
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
