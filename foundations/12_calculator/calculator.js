bd54ed2eb4da9d4ec99597bd9a028892828a0a31
Benjamin Gluzman <benjamin.gluzman@gmail.com>
2025-12-18 20:35:58 -0500

    Complete 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  while(n > 1) {
+    fact *= n;
+    n--;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
