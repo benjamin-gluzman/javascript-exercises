c2af9f273c9c73711e321d0ef7716b85aa14a02d
Benjamin Gluzman <benjamin.gluzman@gmail.com>
2025-12-18 20:59:18 -0500

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0) {
+        return "OOPS";
+    }
+    if(n < 2) {
+        return +n;
+    }
+    let num1 = 0, num2 = 1;
+    let num3;
 
+    for(let i = 1; i < n; i++) {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+    }
+
+    return num3;
 };
 
 // Do not edit below this line
