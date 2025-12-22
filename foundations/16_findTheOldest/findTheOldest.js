06af191756a0d1ecbcb63a4a91ae0f076a47a673
Benjamin Gluzman <benjamin.gluzman@gmail.com>
2025-12-22 14:11:53 -0500

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+        let currentYear = new Date().getFullYear();
 
+        let oldestAge = calculateAge(oldest, currentYear);
+        let currAge = calculateAge(curr, currentYear);
+
+        if(oldestAge >= currAge) return oldest;
+        else return curr;
+    }, people[0])
 };
+
+function calculateAge(person, currentYear) {
+    if(!('yearOfDeath' in person)) return currentYear - person.yearOfBirth;
+    else return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
